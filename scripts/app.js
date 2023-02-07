20228b43f126caa52a72cbfc99fbcfe103501bc6
Shubh Laad <shubhlaad77@gmail.com>
2023-02-08 03:29:52 +0530

    trying to uplaod and show image

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,34 @@
-const clickButton = document.getElementById("clickButton");
-const changedText = document.getElementById("textArea");
+const inputImage = document.getElementById("input-image");
+const outImage = document.getElementById("output-image");
+const uploadButton = document.getElementById("upload-button");
+const attendanceButton = document.getElementById("attendance-button");
 
-function addText() {
-  let textValue = changedText.value;
-  const value = "text...";
-  textValue += value;
-  changedText.value = textValue;
-  console.log(textValue);
-}
+// const toggleDefInputComp = () => {
+//   inputImage.classList.toggle("visible");
+// };
 
-clickButton.addEventListener("click", addText);
+const dislayUploadedImage = (imageSrc) => {
+  const uploadedImage = URL.createObjectURL(imageSrc);
+  const uploadImage = document.querySelector(".uploaded-image");
+  uploadImage.src = uploadedImage;
+};
+
+const uploadImage = () => {
+  let inputImageComp = document.createElement("input");
+  inputImageComp.type = "file";
+  inputImageComp.accept = "image/png, image/jpeg, image/jpg, image/heic";
+  inputImageComp.click();
+
+  inputImageComp.addEventListener =
+    ("change",
+    () => {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        let uploadedImage = reader.result;
+        document.querySelector(".uploaded-image").style.backgroundImage = `url(${uploadedImage})`;
+      });
+      reader.readAsDataURL(this.files[0]);
+      console.log(reader);
+    });
+};
+uploadButton.addEventListener("click", uploadImage);
