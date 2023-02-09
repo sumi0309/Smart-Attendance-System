4dccdd06ba8c2618be540f6e4527a7bf412dd42d
Shubh Laad <shubhlaad77@gmail.com>
2023-02-09 14:10:45 +0530

    correct upload image toggle system

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,17 @@ const outImage = document.querySelector(".output-image");
 const attendanceButton = document.getElementById("attendance-button");
 const deleteButton = document.getElementById("delete-button");
 
+const togglePreview = () => {
+  previewImage.classList.toggle("visible");
+};
+
 const showPreview = (event) => {
   if (event.target.files.length > 0) {
     const imageFile = event.target.files[0];
     const src = URL.createObjectURL(imageFile);
     const preview = document.querySelector("#preview");
     const labelTag = document.querySelector("#preview-label");
+    togglePreview();
     preview.src = src;
     preview.style.display = "inline-block";
     preview.style.objectFit = "contain";
