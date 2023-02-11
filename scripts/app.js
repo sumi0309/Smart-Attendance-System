49eef75c9f6327a3926cedd644422af280476c75
Shubh Laad <shubhladd@197BLRMBT3243.local>
2023-02-11 17:10:42 +0530

    saving image to localStorage and retrieving dataURL

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,17 @@
 const previewImage = document.querySelector(".preview-container");
 const outImage = document.querySelector(".output-image");
 const attendanceButton = document.getElementById("attendance-button");
-// const deleteButton = document.getElementById("delete-button");
 const inputImagePreview = document.querySelector(".image-input");
 const outputImagePreview = document.querySelector(".output-input");
 const preview = document.querySelector("#preview");
 const labelTag = document.querySelector("#preview-label");
 const backdrop = document.querySelector("#backdrop");
 const modalSheet = document.querySelector(".modal-sheet");
+const uploadAction = document.querySelector("#upload");
+
+document.addEventListener("DOMContentLoaded", () => {
+	localStorage.clear();
+});
 
 const togglePreview = () => {
 	previewImage.classList.toggle("visible");
@@ -28,20 +32,46 @@ const toggleModalSheet = () => {
 	modalSheet.classList.toggle("visible");
 };
 
-const showPreview = (event) => {
-	if (event.target.files.length > 0) {
-		const imageFile = event.target.files[0];
-		const src = URL.createObjectURL(imageFile);
+// const showPreview = (event) => {
+// 	if (event.target.files.length > 0) {
+// 		const imageFile = event.target.files[0];
+// 		const src = URL.createObjectURL(imageFile);
+// 		console.log(src);
+// 		togglePreview();
+// 		toggleImageInput();
+// 		preview.src = src;
+// 		preview.style.display = "inline-block";
+// 		preview.style.objectFit = "contain";
+// 		preview.style.borderRadius = "1em";
+// 		labelTag.textContent = imageFile.name;
+// 	}
+// };
 
-		togglePreview();
-		toggleImageInput();
-		preview.src = src;
-		preview.style.display = "inline-block";
-		preview.style.objectFit = "contain";
-		preview.style.borderRadius = "1em";
-		labelTag.textContent = imageFile.name;
-	}
+const showPreview = (event) => {
+	const imageFile = event.target.files[0];
+	const reader = new FileReader();
+
+	reader.readAsDataURL(imageFile);
+	reader.addEventListener("load", () => {
+		localStorage.setItem("latest-image", reader.result);
+		const latestImage = localStorage.getItem("latest-image");
+		preview.src = latestImage;
+	});
+
+	togglePreview();
+	toggleImageInput();
+
+	preview.style.display = "inline-block";
+	preview.style.objectFit = "contain";
+	preview.style.borderRadius = "1em";
+
+	labelTag.textContent = imageFile.name;
 };
+
+uploadAction.addEventListener("change", (event) => {
+	localStorage.clear();
+	showPreview(event);
+});
 
 backdrop.addEventListener("click", () => {
 	toggleBackdrop();
@@ -49,7 +79,8 @@ backdrop.addEventListener("click", () => {
 });
 
 attendanceButton.addEventListener("click", () => {
-	if (inputImagePreview.src === undefined) {
+	const latestImage = localStorage.getItem("latest-image");
+	if (latestImage === null) {
 		toggleBackdrop();
 		toggleModalSheet();
 	}
