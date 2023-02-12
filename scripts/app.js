2375a45098af61855778d5fc5cf9f438d2fa9350
Shubh Laad <shubhladd@197BLRMBT3243.local>
2023-02-12 18:45:50 +0530

    face detection beta model complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,29 @@
-const previewImage = document.querySelector(".preview-container");
-const outImage = document.querySelector(".output-image");
-const attendanceButton = document.getElementById("attendance-button");
 const inputImagePreview = document.querySelector(".image-input");
-const outputImagePreview = document.querySelector(".output-input");
-const preview = document.querySelector("#preview");
-const labelTag = document.querySelector("#preview-label");
-const backdrop = document.querySelector("#backdrop");
-const modalSheet = document.querySelector(".modal-sheet");
+const inputPreviewContainer = document.querySelector(".input-preview-container");
+const inputLabelTag = document.querySelector("#input-preview-label");
+const inputPreview = document.querySelector("#input-preview");
 const uploadAction = document.querySelector("#upload");
+
+Promise.all([
+	faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
+	faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
+	faceapi.nets.ssdMobilenetv1.loadFromUri("../models"),
+	faceapi.nets.mtcnn.loadFromUri("../models"),
+]);
 
 document.addEventListener("DOMContentLoaded", () => {
 	localStorage.clear();
 });
 
-const togglePreview = () => {
-	previewImage.classList.toggle("visible");
-};
-
-const toggleImageInput = () => {
+const toggleInputPreview = () => {
 	inputImagePreview.classList.toggle("visible");
 };
 
-const toggleImageOutput = () => {
-	outputImagePreview.classList.toggle("visible");
+const toggleInputImage = () => {
+	inputPreview.classList.toggle("visible");
 };
 
-const toggleBackdrop = () => {
-	backdrop.classList.toggle("visible");
-};
-const toggleModalSheet = () => {
-	modalSheet.classList.toggle("visible");
-};
-
-// const showPreview = (event) => {
-// 	if (event.target.files.length > 0) {
-// 		const imageFile = event.target.files[0];
-// 		const src = URL.createObjectURL(imageFile);
-// 		console.log(src);
-// 		togglePreview();
-// 		toggleImageInput();
-// 		preview.src = src;
-// 		preview.style.display = "inline-block";
-// 		preview.style.objectFit = "contain";
-// 		preview.style.borderRadius = "1em";
-// 		labelTag.textContent = imageFile.name;
-// 	}
-// };
-
-const showPreview = (event) => {
+const showInputPreview = (event) => {
 	const imageFile = event.target.files[0];
 	const reader = new FileReader();
 
@@ -55,33 +31,20 @@ const showPreview = (event) => {
 	reader.addEventListener("load", () => {
 		localStorage.setItem("latest-image", reader.result);
 		const latestImage = localStorage.getItem("latest-image");
-		preview.src = latestImage;
+		inputPreview.src = latestImage;
 	});
 
-	togglePreview();
-	toggleImageInput();
+	toggleInputImage();
+	toggleInputPreview();
 
-	preview.style.display = "inline-block";
-	preview.style.objectFit = "contain";
-	preview.style.borderRadius = "1em";
+	inputPreview.style.display = "block";
+	inputPreview.style.objectFit = "contain";
+	inputPreview.style.borderRadius = "1em";
 
-	labelTag.textContent = imageFile.name;
+	inputLabelTag.textContent = imageFile.name;
 };
 
 uploadAction.addEventListener("change", (event) => {
 	localStorage.clear();
-	showPreview(event);
-});
-
-backdrop.addEventListener("click", () => {
-	toggleBackdrop();
-	toggleModalSheet();
-});
-
-attendanceButton.addEventListener("click", () => {
-	const latestImage = localStorage.getItem("latest-image");
-	if (latestImage === null) {
-		toggleBackdrop();
-		toggleModalSheet();
-	}
+	showInputPreview(event);
 });
--- scripts/face-recog.js
+++ scripts/face-recog.js
@@ -1,15 +1,103 @@
-const imageUpload = document.querySelector(".image_input");
-const imageResult = document.querySelector(".image_output");
-const labelTag = document.querySelector("#preview-label");
+const outputImagePreview = document.querySelector(".image-output");
+const outputPreviewContainer = document.querySelector(".output-preview-container");
+const outputLabelTag = document.querySelector("#output-preview-label");
+const outputPreview = document.querySelector("#output-preview");
 
-Promise.all([
-  await faceapi.loadSsdMobilenetv1Model("/models"),
-  await faceapi.loadTinyFaceDetectorModel("/models"),
-  await faceapi.loadMtcnnModel("/models"),
-  await faceapi.loadFaceLandmarkModel("/models"),
-  await faceapi.loadFaceLandmarkTinyModel("/models"),
-  await faceapi.loadFaceRecognitionModel("/models"),
-  await faceapi.loadFaceExpressionModel("/models"),
-]).then(start);
+const attendanceButton = document.getElementById("attendance-button");
 
-function start() {}
+const backdrop = document.querySelector("#backdrop");
+const modalSheet = document.querySelector(".modal-sheet");
+
+const toggleOutputPreview = () => {
+	outputImagePreview.classList.toggle("visible");
+};
+
+const toggleOutputImage = () => {
+	outputPreview.classList.toggle("visible");
+};
+
+const toggleBackdrop = () => {
+	backdrop.classList.toggle("visible");
+};
+
+const toggleModalSheet = () => {
+	modalSheet.classList.toggle("visible");
+};
+
+const dataUrlToBlob = (imageDataUrl) => {
+	var byteString = atob(imageDataUrl.split(",")[1]);
+	var mimeString = imageDataUrl.split(",")[0].split(":")[1].split(";")[0];
+	var ab = new ArrayBuffer(byteString.length);
+	var ia = new Uint8Array(ab);
+
+	for (var i = 0; i < byteString.length; i++) {
+		ia[i] = byteString.charCodeAt(i);
+	}
+
+	var blob = new Blob([ab], { type: mimeString });
+	return blob;
+};
+
+const showOutputPreview = (latestImage) => {
+	toggleOutputPreview();
+	toggleOutputImage();
+
+	outputPreview.src = latestImage;
+	outputPreview.style.display = "inline-block";
+	outputPreview.style.objectFit = "contain";
+	outputPreview.style.borderRadius = "1em";
+};
+
+let image;
+let canvas;
+
+const execDetection = async (loadImage) => {
+	if (loadImage === null) labelTag.textContent = "No Image Found";
+	else {
+		if (image) image.remove();
+		if (canvas) canvas.remove();
+
+		const imageBLob = dataUrlToBlob(loadImage);
+		image = await faceapi.bufferToImage(imageBLob);
+
+		canvas = faceapi.createCanvasFromMedia(image);
+		outputPreviewContainer.append(canvas);
+		outputImagePreview.style.justifyContent = "start";
+		outputImagePreview.style.alignItems = "center";
+		outputImagePreview.style.position = "relative";
+
+		const displaySize = {
+			width: outputPreview.width,
+			height: outputPreview.height,
+		};
+		faceapi.matchDimensions(canvas, displaySize);
+
+		const detections = await faceapi
+			.detectAllFaces(image, new faceapi.SsdMobilenetv1Options({ minConfidence: 0.28 }))
+			.withFaceLandmarks()
+			.withFaceDescriptors();
+		outputLabelTag.textContent = detections.length;
+		const resizeDetections = faceapi.resizeResults(detections, displaySize);
+		resizeDetections.forEach((item) => {
+			const box = item.detection.box;
+			const drawBox = new faceapi.draw.DrawBox(box);
+			drawBox.draw(canvas);
+		});
+	}
+};
+
+backdrop.addEventListener("click", () => {
+	toggleBackdrop();
+	toggleModalSheet();
+});
+
+attendanceButton.addEventListener("click", () => {
+	const latestImage = localStorage.getItem("latest-image");
+	if (latestImage === null) {
+		toggleBackdrop();
+		toggleModalSheet();
+	} else {
+		showOutputPreview(latestImage);
+		execDetection(latestImage);
+	}
+});
