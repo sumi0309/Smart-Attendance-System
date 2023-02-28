f9dbe62eacfa4594a1e023c43c9be8b842ab44d6
Shubh Laad <shubhlaad77@gmail.com>
2023-02-28 19:43:12 +0530

    complete code upto multi-image output

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,5 @@
 const inputImagePreview = document.querySelector(".image-input");
-const inputPreviewContainer = document.querySelector(
-  ".input-preview-container"
-);
+const inputPreviewContainer = document.querySelector(".input-preview-container");
 const attendanceBtn = document.querySelector("#upload-button");
 const attendanceList = document.getElementById("aL");
 const inputLabelTag = document.querySelector("#input-preview-label");
@@ -27,29 +25,82 @@ const toggleInputImage = () => {
   inputPreview.classList.toggle("visible");
 };
 
-const showInputPreview = (event) => {
-  const imageFile = event.target.files[0];
-  const reader = new FileReader();
-
-  reader.readAsDataURL(imageFile);
-  reader.addEventListener("load", () => {
-    inputPreview.src = reader.result;
-    execDetection(reader.result);
-    showOutputPreview(reader.result);
-  });
-
-  toggleInputImage();
-  toggleInputPreview();
-
-  inputPreview.style.display = "block";
-  inputPreview.style.objectFit = "contain";
-  inputPreview.style.borderRadius = "1em";
-
-  inputLabelTag.textContent = imageFile.name;
+const removeDuplicates = async (arr) => {
+  return arr.filter((item, index) => arr.indexOf(item) === index);
 };
 
-uploadAction.addEventListener("change", (event) => {
+let imageFile = [];
+let finalList = [];
+let studentData = [];
+
+uploadAction.addEventListener("change", async (event) => {
+  if (event.target.files.length === 0) return;
+
+  while (imageFile.length !== 0) imageFile.pop();
+  while (finalList.length !== 0) finalList.pop();
+  inputPreviewContainer.innerHTML = "";
   attendanceList.innerHTML = "";
+  if (canvas) await canvas.remove();
+  if (image) await image.remove();
+
+  for (let i = 0; i < event.target.files.length; i++) {
+    imageFile.push(event.target.files[i]);
+  }
+
+  for (let i of imageFile) {
+    const childImage = await faceapi.bufferToImage(i);
+    const childImageLabel = document.createElement("p");
+
+    childImage.setAttribute("id", "input-preview");
+    childImage.style.display = "block";
+    childImage.style.objectFit = "contain";
+    childImage.style.borderRadius = "1em";
+    inputPreviewContainer.appendChild(childImage);
+
+    childImageLabel.setAttribute("id", "input-preview-label");
+    childImageLabel.textContent = i.name;
+    childImageLabel.style.textAlign = "center";
+    childImageLabel.style.marginTop = "0.3em";
+    childImageLabel.style.marginBottom = "0.3em";
+    inputPreviewContainer.appendChild(childImageLabel);
+  }
+  inputLabelTag.textContent = "";
+  outputPreviewContainer.innerHTML = "";
   outputLabelTag.textContent = "";
-  showInputPreview(event);
+
+  studentData = await loadLabeledImages();
+
+  for (let i = 0; i < imageFile.length; i++) {
+    await execDetection(imageFile[i], i);
+  }
+
+  const allCanvas = document.querySelectorAll("canvas");
+  // const holders = document.querySelectorAll(".image-holder");
+
+  for (let i of allCanvas) {
+    i.style.position = "relative";
+    i.style.bottom = `${i.height + 30}`;
+  }
+
+  // for (let i of holders) {
+  //   i.style.height = `${i.firstChild.height + 30}`;
+  // }
+
+  const listElems = document.querySelector("#aL").children;
+  for (let i = 0; i < listElems.length; i++) {
+    let string = listElems[i].innerText;
+    string = string.substring(0, string.indexOf("("));
+    finalList.push(string);
+  }
+
+  finalList.sort();
+  document.querySelector("#aL").innerHTML = "";
+  finalList = await removeDuplicates(finalList);
+
+  for (let i = 0; i < finalList.length; i++) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(finalList[i]));
+    attendanceList.appendChild(li);
+  }
+  // alert("Processing Complete\nClick on the attendance button to get the attendance!");
 });
--- scripts/face-recog.js
+++ scripts/face-recog.js
@@ -1,9 +1,7 @@
 const outputImagePreview = document.querySelector(".image-output");
-const outputPreviewContainer = document.querySelector(
-  ".output-preview-container"
-);
+const outputPreviewContainer = document.querySelector(".output-preview-container");
 const outputLabelTag = document.querySelector("#output-preview-label");
-const outputPreview = document.querySelector("#output-preview");
+const outputPreview = document.querySelectorAll("#output-preview");
 const attendanceButton = document.getElementById("attendance-button");
 const aLDiv = document.querySelector("#aLContainer");
 const aL = document.getElementById("aL");
@@ -34,92 +32,80 @@ const toggleModalSheet = () => {
   modalSheet.classList.toggle("visible");
 };
 
-const dataUrlToBlob = (imageDataUrl) => {
-  var byteString = atob(imageDataUrl.split(",")[1]);
-  var mimeString = imageDataUrl.split(",")[0].split(":")[1].split(";")[0];
-  var ab = new ArrayBuffer(byteString.length);
-  var ia = new Uint8Array(ab);
-
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  var blob = new Blob([ab], { type: mimeString });
-  return blob;
-};
-
-const showOutputPreview = (latestImage) => {
-  toggleOutputPreview();
-  toggleOutputImage();
-
-  outputPreview.src = latestImage;
-  outputPreview.style.display = "inline-block";
-  outputPreview.style.objectFit = "contain";
-  outputPreview.style.borderRadius = "1em";
-};
+const drawBoxes = async () => {};
 
 let image;
 let canvas;
+const sortedList = [];
 
-const execDetection = async (loadImage) => {
-  if (loadImage === null) labelTag.textContent = "No Image Found";
+const execDetection = async (loadImage, index) => {
+  if (loadImage === null) outputLabelTag.textContent = "No Image Found";
   else {
-    if (image) image.remove();
-    if (canvas) canvas.remove();
+    while (sortedList.length !== 0) {
+      sortedList.pop();
+    }
 
-    const imageBLob = dataUrlToBlob(loadImage);
-    image = await faceapi.bufferToImage(imageBLob);
-
-    canvas = faceapi.createCanvasFromMedia(image);
+    image = await faceapi.bufferToImage(loadImage);
+    const imageLabel = document.createElement("p");
+    canvas = await faceapi.createCanvasFromMedia(image);
     outputImagePreview.style.justifyContent = "start";
     outputImagePreview.style.alignItems = "center";
     outputImagePreview.style.position = "relative";
 
-    const labeledFaceDescriptors = await loadLabeledImages();
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.53);
+    const holder = document.createElement("div");
+    holder.setAttribute("id", `holder${index}`);
+    holder.setAttribute("class", "image-holder");
+    outputPreviewContainer.appendChild(holder);
 
+    image.setAttribute("id", "output-preview");
+    image.style.display = "block";
+    image.style.objectFit = "contain";
+    image.style.borderRadius = "1em";
+    document.querySelector(`#holder${index}`).appendChild(image);
+
+    imageLabel.setAttribute("id", "output-preview-label");
+    imageLabel.textContent = `output_${loadImage.name}`;
+    imageLabel.style.textAlign = "center";
+    imageLabel.style.marginTop = "0.3em";
+    imageLabel.style.marginBottom = "0.3em";
+    document.querySelector(`#holder${index}`).appendChild(imageLabel);
+
+    const faceMatcher = await new faceapi.FaceMatcher(studentData, 0.53);
     const displaySize = {
-      width: outputPreview.width,
-      height: outputPreview.height,
+      width: document.querySelectorAll("#output-preview")[index].width,
+      height: document.querySelectorAll("#output-preview")[index].height,
     };
-    faceapi.matchDimensions(canvas, displaySize);
+    await faceapi.matchDimensions(canvas, displaySize);
+    console.log(displaySize);
 
     const detections = await faceapi
-      .detectAllFaces(
-        image,
-        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.33 })
-      )
+      .detectAllFaces(image, new faceapi.SsdMobilenetv1Options({ minConfidence: 0.33 }))
       .withFaceLandmarks()
       .withFaceDescriptors();
-    outputLabelTag.textContent = detections.length;
-    const resizeDetections = faceapi.resizeResults(detections, displaySize);
+    const resizeDetections = await faceapi.resizeResults(detections, displaySize);
+    const results = await resizeDetections.map((d) => faceMatcher.findBestMatch(d.descriptor));
+    console.log(resizeDetections);
+    console.log(results);
 
-    const results = resizeDetections.map((d) =>
-      faceMatcher.findBestMatch(d.descriptor)
-    );
-
-    const sortedList = [];
-
-    results.forEach((student) => {
+    await results.forEach((student) => {
       sortedList.push(student.toString());
     });
 
-    sortedList.sort();
-
-    results.forEach((result, i) => {
+    await results.forEach((result, i) => {
       const box = resizeDetections[i].detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, {
-        label: result.toString(),
-      });
+      const drawBox = new faceapi.draw.DrawBox(box);
       drawBox.draw(canvas);
     });
+    document.querySelector(`#holder${index}`).appendChild(canvas);
+
+    sortedList.sort();
+
     for (let i = 0; i < sortedList.length; i++) {
       var li = document.createElement("li");
       li.appendChild(document.createTextNode(sortedList[i]));
       attendanceList.appendChild(li);
     }
   }
-  outputPreviewContainer.append(canvas);
 };
 
 backdrop.addEventListener("click", () => {
@@ -128,67 +114,8 @@ backdrop.addEventListener("click", () => {
 });
 
 attendanceButton.addEventListener("click", () => {
-  if (document.getElementById("aL").childNodes.length === 0) {
+  if (document.getElementById("aL").children.length === 0) {
     toggleBackdrop();
     toggleModalSheet();
   } else toogleAttendanceList();
 });
-
-function loadLabeledImages() {
-  const labels = [
-    "18T7121_Ekta Bara",
-    "19T7104_Abhishek Thakur",
-    "19T7105_Aditya Singh Dangi",
-    "19T7109_Antriksh Tyagi",
-    "19T7110_Anubhav Chahar",
-    "19T7112_Ayush Agrawal",
-    // "19T7113_Bharti Baghel",
-    "19T7114_Bhumika Modh",
-    // "19T7115_Daniya Afaq",
-    "19T7117_Deepesh Aswani",
-    "19T7118_Divyanshu Parwal",
-    "19T7121_Gourav Dehariya",
-    "19T7122_Hitesh Mani",
-    "19T7127_Kushagra Bandil",
-    // "19T7128_Lakshya Khare",
-    "19T7132_Mihir Jain",
-    "19T7137_Pranjal Sahu",
-    "19T7138_Pranshu Jain",
-    "19T7139_Prashant Bairagi",
-    "19T7142_Priyanshu Khare",
-    "19T7144_Rajat Hotwani",
-    "19T7149_Sanidhya Pal",
-    "19T7153_Shakti Aggarwal",
-    "19T7154_Shraddha Soni",
-    "19T7156_Shubh Laad",
-    "19T7157_Siddhant Jain",
-    "19T7158_Sumiran Jaiswal",
-    // "19T7160_Swapnil Janoria",
-    "19T7163_Urvish Jain",
-    "19T7164_Utkarsh Gupte",
-    // "19T7165_Vidushi Kumre",
-    "19T7167_Vishal Yadav",
-    "19T7170_Yogesh Jadon",
-    "19T7171_Yuvraj Singh Shaktawat",
-    "19T7172_Hussain Barwahwala",
-    "19T7184_Nayan Bargal",
-    "20T7182_Jayesh Mulchandani",
-    "20T7184_Nisha Chilgar",
-    "19T7010_Anjali Phadke",
-  ];
-
-  return Promise.all(
-    labels.map(async (label) => {
-      const descriptions = [];
-      for (let i = 1; i <= 1; i++) {
-        const img = await faceapi.fetchImage(`labeledImages/${label}.jpg`);
-        const detections = await faceapi
-          .detectSingleFace(img)
-          .withFaceLandmarks()
-          .withFaceDescriptor();
-        descriptions.push(detections.descriptor);
-      }
-      return new faceapi.LabeledFaceDescriptors(label, descriptions);
-    })
-  );
-}
