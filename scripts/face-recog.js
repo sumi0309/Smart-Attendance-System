97c163e913321c14b7d1a92f7ceb6aac31b267b1
Your Name <you@example.com>
2023-03-06 14:31:23 +0530

    latest code with full functionality

--- scripts/face-recog.js
+++ scripts/face-recog.js
@@ -34,51 +34,11 @@ const toggleModalSheet = () => {
   modalSheet.classList.toggle("visible");
 };
 
-<<<<<<< HEAD
-const dataUrlToBlob = async (imageDataUrl) => {
-  var byteString = atob(imageDataUrl.split(",")[1]);
-  var mimeString = imageDataUrl.split(",")[0].split(":")[1].split(";")[0];
-  var ab = new ArrayBuffer(byteString.length);
-  var ia = new Uint8Array(ab);
-
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  var blob = new Blob([ab], { type: mimeString });
-  return blob;
-};
-
-const showOutputPreview = async (latestImage) => {
-  toggleOutputPreview();
-  toggleOutputImage();
-
-  outputPreview.src = latestImage;
-  outputPreview.style.display = "inline-block";
-  outputPreview.style.objectFit = "contain";
-  outputPreview.style.borderRadius = "1em";
-};
-=======
 const drawBoxes = async () => {};
->>>>>>> main
 
 let image;
 let canvas;
 const sortedList = [];
-<<<<<<< HEAD
-
-const execDetection = async (loadImage) => {
-  while (sortedList.length != 0) {
-    sortedList.pop();
-  }
-  if (loadImage === null) outputLabelTag.textContent = "No Image Found";
-  else {
-    if (image) await image.remove();
-    if (canvas) await canvas.remove();
-
-    // const imageBLob = await dataUrlToBlob(loadImage);
-    image = await faceapi.bufferToImage(loadImage);
-=======
 var totalOutputHeight = 0;
 
 const execDetection = async (loadImage, index) => {
@@ -90,19 +50,11 @@ const execDetection = async (loadImage, index) => {
 
     image = await faceapi.bufferToImage(loadImage);
     const imageLabel = document.createElement("p");
->>>>>>> main
     canvas = await faceapi.createCanvasFromMedia(image);
     outputImagePreview.style.justifyContent = "start";
     outputImagePreview.style.alignItems = "center";
     outputImagePreview.style.position = "relative";
 
-<<<<<<< HEAD
-    const labeledFaceDescriptors = await loadLabeledImages();
-    const faceMatcher = await new faceapi.FaceMatcher(
-      labeledFaceDescriptors,
-      0.53
-    );
-=======
     const holder = document.createElement("div");
     holder.setAttribute("id", `holder${index}`);
     holder.setAttribute("class", "image-holder");
@@ -118,7 +70,6 @@ const execDetection = async (loadImage, index) => {
     // document
     //   .querySelector(`#holder${index}`)
     //   .setAttribute("style", `height:${imgH + 30};`);
->>>>>>> main
 
     // totalOutputHeight += imgH + 30;
 
@@ -135,10 +86,7 @@ const execDetection = async (loadImage, index) => {
       height: document.querySelectorAll("#output-preview")[index].height,
     };
     await faceapi.matchDimensions(canvas, displaySize);
-<<<<<<< HEAD
-=======
     // console.log(displaySize);
->>>>>>> main
 
     const detections = await faceapi
       .detectAllFaces(
@@ -147,18 +95,10 @@ const execDetection = async (loadImage, index) => {
       )
       .withFaceLandmarks()
       .withFaceDescriptors();
-<<<<<<< HEAD
-    // outputLabelTag.textContent = detections.length;
-=======
->>>>>>> main
     const resizeDetections = await faceapi.resizeResults(
       detections,
       displaySize
     );
-<<<<<<< HEAD
-
-=======
->>>>>>> main
     const results = await resizeDetections.map((d) =>
       faceMatcher.findBestMatch(d.descriptor)
     );
@@ -169,20 +109,6 @@ const execDetection = async (loadImage, index) => {
       sortedList.push(student.toString());
     });
 
-<<<<<<< HEAD
-    // results.forEach((result, i) => {
-    //   const box = resizeDetections[i].detection.box;
-    //   const drawBox = new faceapi.draw.DrawBox(box, {
-    //     label: result.toString(),
-    //   });
-    //   drawBox.draw(canvas);
-    // });
-    // for (let i = 0; i < sortedList.length; i++) {
-    //   var li = document.createElement("li");
-    //   li.appendChild(document.createTextNode(sortedList[i]));
-    //   attendanceList.appendChild(li);
-    // }
-=======
     await results.forEach((result, i) => {
       const box = resizeDetections[i].detection.box;
       const drawBox = new faceapi.draw.DrawBox(box);
@@ -197,7 +123,6 @@ const execDetection = async (loadImage, index) => {
       li.appendChild(document.createTextNode(sortedList[i]));
       attendanceList.appendChild(li);
     }
->>>>>>> main
   }
 };
 
@@ -212,66 +137,3 @@ attendanceButton.addEventListener("click", () => {
     toggleModalSheet();
   } else toogleAttendanceList();
 });
-<<<<<<< HEAD
-
-function loadLabeledImages() {
-  const labels = [
-    "18T7121_Ekta Bara",
-    "19T7104_Abhishek Thakur",
-    "19T7105_Aditya Singh Dangi",
-    "19T7109_Antriksh Tyagi",
-    "19T7110_Anubhav Chahar",
-    "19T7112_Ayush Agrawal",
-    // "19T7113_Bharti Baghel",
-    "19T7114_Bhumika Modh",
-    // "19T7115_Daniya Afaq",
-    "19T7117_Deepesh Aswani",
-    "19T7118_Divyanshu Parwal",
-    "19T7121_Gourav Dehariya",
-    "19T7122_Hitesh Mani",
-    "19T7127_Kushagra Bandil",
-    // "19T7128_Lakshya Khare",
-    "19T7132_Mihir Jain",
-    "19T7137_Pranjal Sahu",
-    "19T7138_Pranshu Jain",
-    "19T7139_Prashant Bairagi",
-    "19T7142_Priyanshu Khare",
-    "19T7144_Rajat Hotwani",
-    "19T7149_Sanidhya Pal",
-    "19T7153_Shakti Aggarwal",
-    "19T7154_Shraddha Soni",
-    "19T7156_Shubh Laad",
-    "19T7157_Siddhant Jain",
-    "19T7158_Sumiran Jaiswal",
-    // "19T7160_Swapnil Janoria",
-    "19T7163_Urvish Jain",
-    "19T7164_Utkarsh Gupte",
-    // "19T7165_Vidushi Kumre",
-    "19T7167_Vishal Yadav",
-    "19T7170_Yogesh Jadon",
-    "19T7171_Yuvraj Singh Shaktawat",
-    "19T7172_Hussain Barwahwala",
-    "19T7184_Nayan Bargal",
-    "20T7182_Jayesh Mulchandani",
-    "20T7184_Nisha Chilgar",
-    "19T7005_Anjali Phadke",
-    "007_Monu Bhaiya",
-  ];
-
-  return Promise.all(
-    labels.map(async (label) => {
-      const descriptions = [];
-      for (let i = 1; i <= 1; i++) {
-        const img = await faceapi.fetchImage(`labeledImages/${label}.jpg`);
-        const detections = await faceapi
-          .detectSingleFace(img)
-          .withFaceLandmarks()
-          .withFaceDescriptor();
-        descriptions.push(detections.descriptor);
-      }
-      return new faceapi.LabeledFaceDescriptors(label, descriptions);
-    })
-  );
-}
-=======
->>>>>>> main
